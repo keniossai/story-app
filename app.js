7ec08998c6a3c1ca3ee2ba9ada0a8c840eb8487f
Kenneth Ossai <kenossai@ymail.com>
2021-05-15 08:18:21 +0100

    Created edit story page, added PUT request and edit functionality

--- app.js
+++ app.js
@@ -4,12 +4,12 @@ const mongoose = require('mongoose')
 const dotenv = require('dotenv')
 const passport = require('passport')
 const session = require('express-session')
+const methodOverride = require('method-override')
 // Morgan for login
 const morgan = require('morgan')
 const exphbs = require('express-handlebars')
 const MongoStore = require('connect-mongo')
 const connectDB = require('./config/db')
-const { urlencoded } = require('express')
 
 // Load Config
 dotenv.config({ path: './config/config.env' })
@@ -25,18 +25,39 @@ const app = express()
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
 
+// Method Override
+app.use(
+	methodOverride(function (req, res) {
+		if (req.body && typeof req.body === 'object' && '_method' in req.body) {
+			// look in urlencoded POST bodies and delete it
+			let method = req.body._method
+			delete req.body._method
+			return method
+		}
+	})
+)
+
 if (process.env.NODE_ENV === 'development') {
 	app.use(morgan('dev'))
 }
 
 // Handlebars Helper
-const { formatDate, stripTags, truncate } = require('./helpers/hbs')
+const {
+	formatDate,
+	stripTags,
+	truncate,
+	editIcon,
+	select,
+} = require('./helpers/hbs')
 
 // Handlebars
-
 app.engine(
 	'.hbs',
-	exphbs({ helpers: { formatDate, stripTags, truncate, }, defaultLayout: 'main', extname: '.hbs' })
+	exphbs({
+		helpers: { formatDate, stripTags, truncate, editIcon, select },
+		defaultLayout: 'main',
+		extname: '.hbs',
+	})
 )
 app.set('view engine', '.hbs')
 
@@ -61,6 +82,12 @@ app.use(
 // Paasport Middleware
 app.use(passport.initialize())
 app.use(passport.session())
+
+// Set global variable
+app.use(function (req, res, next) {
+	res.locals.user = req.user || null
+	next()
+})
 
 // Static Folder
 app.use(express.static(path.join(__dirname, 'public')))
